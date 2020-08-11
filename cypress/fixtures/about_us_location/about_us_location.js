0c93fb79e461b3e00dc761449f45890076c09058
dalian98 <13458017601@163.com>
2020-08-11 15:36:46 +0800

    增加关于我们模块用例及定位信息

---
+++ cypress/fixtures/about_us_location/about_us_location.js
@@ -0,0 +1,15 @@
+//关于我们界面元素定位信息
+export const about_us ={
+    INTRODUCTION:'.title___1U9Fw',//平台介绍标题
+    notice:'.ant-radio-group > :nth-child(2) > :nth-child(2)',//通知公告
+    notice_first:'[data-row-key="0"] > td > .list_itembox > .item_main___VNGxK',//第一条公告
+    notice_first_back:'[style="margin-top: 40px;"] > :nth-child(1)',//返回到公告通知列表
+    contact_information:'.ant-radio-group > :nth-child(3) > :nth-child(2)',//联系方式
+    query_input:'.ant-input',//联系方式界面输入框
+    query_btn:'.ant-input-group-addon > .ant-btn',//联系方式界面查询按钮
+    administrator_mailbox:'.ant-radio-group > :nth-child(4) > :nth-child(2)',//管理员信箱
+    messageContent:'#messageContent',//管理员信箱输入框
+    commit_btn:'.ant-col-18 > .ant-btn',//管理员信息提交按钮
+    common_problem:'.ant-radio-group > :nth-child(5) > :nth-child(2)',//常见问题
+    common_problem_1:'.title___2ihOG',//如何注册个人账号？
+}
---
+++ cypress/integration/about_us/about_us.js
@@ -0,0 +1,35 @@
+import {about_us} from '../../fixtures/about_us_location/about_us_location'
+
+
+describe('关于我们模块测试用例集',function(){
+    beforeEach('进入关于我们界面',function(){
+        cy.visit('http://192.168.10.112/cui/about')
+    })
+    it('平台介绍',function(){
+        cy.get(about_us.INTRODUCTION).should('have.text','平台介绍')
+    })
+    it('通知公告',function(){
+        cy.get(about_us.notice).click()
+        cy.get(about_us.notice_first).click()
+        cy.get(about_us.notice_first_back).click()
+        cy.get(about_us.notice_first).should('exist')
+    })
+    it('联系方式',function(){
+        cy.get(about_us.contact_information).click()
+        cy.wait(3000)
+        cy.get(about_us.query_input).type('成都')
+        cy.get(about_us.query_btn).click()
+    })
+    it('管理员信箱（未登陆）',function(){
+        cy.get(about_us.administrator_mailbox).click()
+        cy.get(about_us.messageContent).type('giao')
+        cy.get(about_us.commit_btn).click()
+        cy.url().should('eql','http://192.168.10.112/cui/login')
+    })
+    it.only('常见问题',function(){
+        cy.get(about_us.common_problem).click()
+        cy.get(about_us.common_problem_1).should('have.text','如何注册个人账号？')
+
+    })
+
+})
