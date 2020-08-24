1fb56a6e036cd155416cfc9b89984c715a4116ec
dalian98 <13458017601@163.com>
2020-08-24 11:18:53 +0800

    完善政策服务模块测试用例

--- cypress/fixtures/policy_services/policy_services_loction.js
+++ cypress/fixtures/policy_services/policy_services_loction.js
@@ -1,5 +1,16 @@
 export const policy_services ={
     query_input:'.ant-input',//搜索输入框
     query_btn:'.ant-input-suffix > .ant-btn',//搜索按钮
-    result_1:'.ant-list-items >:nth-child(1) .ant-list-item > .ant-list-item-meta > .ant-list-item-meta-content > .ant-list-item-meta-title > div > [style="font-weight: bold;"]'
+    result_1:'.ant-list-items >:nth-child(1) .ant-list-item > .ant-list-item-meta > .ant-list-item-meta-content > .ant-list-item-meta-title > div > [style="font-weight: bold;"]',//搜索结果第一列
+    more_1:':nth-child(1) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//最新中央政策查看更多
+    more_2:':nth-child(1) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px; flex: 1 1 0%;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//最新市洲政策查看更多
+    more_3:':nth-child(2) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//债券融资中央政策查看更多
+    more_4:':nth-child(2) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px; flex: 1 1 0%;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//债券融资省级政策查看更多
+    more_5:':nth-child(3) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//财金政策中央政策查看更多
+    more_6:':nth-child(3) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px; flex: 1 1 0%;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//财金政策省级政策查看更多
+    more_7:':nth-child(4) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//综合政策中央政策查看更多
+    more_8:':nth-child(4) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px; flex: 1 1 0%;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//综合政策省级政策查看更多
+    more_9:':nth-child(5) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//信贷融资中央政策查看更多
+    more_10:':nth-child(5) > .ant-row-flex > [style="padding-left: 8px; padding-right: 8px; flex: 1 1 0%;"] > .ant-card > .ant-card-head > .ant-card-head-wrapper > .ant-card-extra > a',//信贷融资市洲政策查看更多
+    title:':nth-child(2) > .ant-breadcrumb-link',//
 }
--- cypress/integration/policy_services/policy_services.js
+++ cypress/integration/policy_services/policy_services.js
@@ -1,10 +1,61 @@
 import {policy_services} from '../../fixtures/policy_services/policy_services_loction'
 
 describe('政策服务测试用例集',function(){
-    it('搜索政策信息',function(){
+    this.beforeEach('访问政策服务界面',function(){
         cy.visit('http://192.168.10.112/cui/policy')
+    })
+    it('搜索政策信息',function(){
         cy.get(policy_services.query_input).type('关于')
         cy.get(policy_services.query_btn).click()
         cy.get(policy_services.result_1).should('contain.text','关于')
     })
+    it('查看政策服务详情', () => {
+        cy.get(policy_services.query_input).type('关于')
+        cy.get(policy_services.query_btn).click()
+        cy.get(policy_services.result_1).should('contain.text','关于')
+        cy.get(policy_services.result_1).click()
+        cy.url().should('contain','cui/policy/detail?id=')
+    })
+    it('中央查看更多', () => {
+        cy.get(policy_services.more_1).click()
+        cy.get(policy_services.title).should('contain','中央政策')
+    })
+    it('市州查看更多', () => {
+        cy.get(policy_services.more_2).click()
+        cy.get(policy_services.title).should('contain','市州政策')
+    })
+    it('债券融资中央查看更多', () => {
+        cy.get(policy_services.more_3).click()
+        cy.get(policy_services.title).should('contain','债券融资 - 中央政策')
+    })
+    it('债券融资市州查看更多', () => {
+        cy.get(policy_services.more_4).click()
+        cy.get(policy_services.title).should('contain','债券融资 - 省级政策')
+    })
+    it('财金互动中央查看更多', () => {
+        cy.get(policy_services.more_5).click()
+        cy.get(policy_services.title).should('contain','财金互动 - 中央政策')
+    })
+    it('财金互动省级查看更多', () => {
+        cy.get(policy_services.more_6).click()
+        cy.get(policy_services.title).should('contain','财金互动 - 省级政策')
+    })
+    it('综合政策中央查看更多', () => {
+        cy.get(policy_services.more_7).click()
+        cy.get(policy_services.title).should('contain','综合政策 - 中央政策')
+    })
+    it('综合政策市州查看更多', () => {
+        cy.get(policy_services.more_8).click()
+        cy.get(policy_services.title).should('contain','综合政策 - 省级政策')
+    })
+    it('信贷融资中央查看更多', () => {
+        cy.get(policy_services.more_9).click()
+        cy.get(policy_services.title).should('contain','信贷融资 - 中央政策')
+    })
+    it('信贷融资市州查看更多', () => {
+        cy.get(policy_services.more_10).click()
+        cy.get(policy_services.title).should('contain','信贷融资 - 市州政策')
+    })
+
+
 })
